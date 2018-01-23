2fdeaa9977609a90958d403ffbfb60a476133c39
Aman Sharma <aman8696.56@gmail.com>
2018-01-23 17:12:42 +0530

    Here the animation is resetted on next and prev button and the new participant option is added

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var myData;
 
     var ref= firebase.database().ref();
 
-    ref.on('value', function(snapshot) {
+    ref.once('value', function(snapshot) {
         myData = snapshot.val();
     
         document.getElementById("question").innerHTML = myData[0].question;
@@ -24,13 +24,23 @@ $("#next").on("click", function () {
     console.log(c);
     ref = firebase.database().ref();
     
-        document.getElementById("question").innerHTML = myData[c].question;
-        document.getElementById("op1").innerHTML = myData[c].option1;
-        document.getElementById("op2").innerHTML = myData[c].option2;
-        document.getElementById("op3").innerHTML = myData[c].option3;
-        document.getElementById("op4").innerHTML = myData[c].option4;
-        answer = myData[c].answer;
+    ref.on('value', function(snapshot) {
+    
+        document.getElementById("question").innerHTML = snapshot.child(c + "/question").val();
+        document.getElementById("op1").innerHTML = snapshot.child(c + "/option1").val();
+        document.getElementById("op2").innerHTML = snapshot.child(c + "/option2").val();
+        document.getElementById("op3").innerHTML = snapshot.child(c + "/option3").val();
+        document.getElementById("op4").innerHTML = snapshot.child(c + "/option4").val();
+        answer = snapshot.child(c + "/answer").val();
+    });
 
+    //stop animation
+    $(".wrapper .spinner").css("animation", "none");
+    $(".wrapper .filler").css("animation", "none");
+    $(".wrapper .mask").css("animation", "none");
+    
+    
+    
     document.getElementById("op-image1").src = 'images/blue.png';
     document.getElementById("op-image2").src = 'images/blue.png';
     document.getElementById("op-image3").src = 'images/blue.png';
@@ -42,13 +52,21 @@ $("#previous").on("click", function () {
     c--;
     console.log(c);
     ref= firebase.database().ref();
+    ref.on('value', function(snapshot) {
     
-        document.getElementById("question").innerHTML = myData[c].question;
-        document.getElementById("op1").innerHTML = myData[c].option1;
-        document.getElementById("op2").innerHTML = myData[c].option2;
-        document.getElementById("op3").innerHTML = myData[c].option3;
-        document.getElementById("op4").innerHTML = myData[c].option4;
-        answer = myData[c].answer;
+        document.getElementById("question").innerHTML = snapshot.child(c + "/question").val();
+        document.getElementById("op1").innerHTML = snapshot.child(c + "/option1").val();
+        document.getElementById("op2").innerHTML = snapshot.child(c + "/option2").val();
+        document.getElementById("op3").innerHTML = snapshot.child(c + "/option3").val();
+        document.getElementById("op4").innerHTML = snapshot.child(c + "/option4").val();
+        answer = snapshot.child(c + "/answer").val();
+    });
+    
+    
+    //stop animation
+    $(".wrapper .spinner").css("animation", "none");
+    $(".wrapper .filler").css("animation", "none");
+    $(".wrapper .mask").css("animation", "none");
     
     document.getElementById("op-image1").src = 'images/blue.png';
     document.getElementById("op-image2").src = 'images/blue.png';
@@ -84,6 +102,11 @@ $("#icons a:nth-child(3)").on("click", function(){
 });
 
 
+$("#icons a:nth-child(4)").on("click", function(){
+    document.getElementById("call").src = '';
+    document.getElementById("dd").src = '';
+    document.getElementById("skip").src = '';
+});
 
 
 $("#pause").on("click", function(){
@@ -94,6 +117,14 @@ $("#pause").on("click", function(){
 
 
 $("#play").on("click", function(){
+    // restart animation
+    setTimeout(function() {
+        
+    $(".wrapper .spinner").css("animation", "rota 30s linear 1");
+    $(".wrapper .filler").css("animation", "opa 30s steps(1, end) 1 reverse");
+    $(".wrapper .mask").css("animation", "opa 30s steps(1, end) 1");
+    }, 10);
+    
     $(".wrapper .spinner").css("animation-play-state", "running");
     $(".wrapper .filler").css("animation-play-state", "running");
     $(".wrapper .mask").css("animation-play-state", "running");
